c8e50ab803e333e5411a4432b3f9b90bf21ffc53
LuisRCP <160691716+LuisRCP@users.noreply.github.com>
2024-07-23 18:34:30 -0600

    row.c

---
+++ row.c
@@ -0,0 +1,13 @@
+#include "editor.h"
+
+void editorUpdateRow(erow *row) {
+  // Implement the row update logic here
+}
+
+void editorAppendRow(char *s, size_t len) {
+  // Implement the row appending logic here
+}
+
+void editorFreeRow(erow *row) {
+  // Implement the row free logic here
+}
