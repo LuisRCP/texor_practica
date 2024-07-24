e61185399b53855169fe2100e6ae845b4b71a5e5
LuisRCP <160691716+LuisRCP@users.noreply.github.com>
2024-07-23 18:34:53 -0600

    editor.c

---
+++ editor.c
@@ -0,0 +1,17 @@
+#include "editor.h"
+
+void editorRefreshScreen() {
+  // Implement the screen refresh logic here
+}
+
+void editorOpen(char *filename) {
+  // Implement file opening logic here
+}
+
+void editorSave() {
+  // Implement file saving logic here
+}
+
+void editorFind() {
+  // Implement find functionality here
+}
